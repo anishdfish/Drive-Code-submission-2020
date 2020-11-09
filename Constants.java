e3cb9e433c1a297ebcdffc0fd1d180d5f5145d7e
Anishdfish <60631143+anishdfish@users.noreply.github.com>
2020-11-08 20:04:26 -0500

    Add files via upload

---
+++ Constants.java
@@ -0,0 +1,18 @@
+package frc.robot;
+
+public class Constants {
+
+    public static class Hardware {
+
+        public static final int DRIVE_LEFT_CHANNEL = 1;
+        public static final int DRIVE_RIGHT_CHANNEL = 0;
+
+    }
+
+
+    public static class Joysticks {
+        public static final int LEFT_JOYSTICK_PORT = 0;
+        public static final int RIGHT_JOYSTICK_PORT = 1;
+    }
+
+}
